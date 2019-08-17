0c3176a44fc9a009e7c362eff8d973dbdf8d22db
Katsuhiro Morishita <morimori.ynct@gmail.com>
2019-08-17 13:47:30 +0900

    インデントを半角スペースに変更

--- rename.py
+++ rename.py
@@ -8,8 +8,8 @@ files = glob.glob("*.jpg")  # ファイル一覧を取得
 files = sorted(files)       # 名前順にソート
 
 for i in range(len(files)):
-	file = files[i]
-	if "no" in file:
-		continue
-	new_name = "no{0}.jpg".format(i+1)
-	shutil.copyfile(file, new_name)
+    file = files[i]
+    if "no" in file:
+        continue
+    new_name = "no{0}.jpg".format(i+1)
+    shutil.copyfile(file, new_name)
